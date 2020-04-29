1cb0ff9fe2b9ad0d9e70581aecb824e2d3af0b09
Edin Jašarević <edinjasar14@gmail.com>
2020-04-29 17:43:06 +0200

    exploring an identity provider

---
+++ ConfArch.IdentityProvider/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace ConfArch.IdentityProvider.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime CareerStartedDate { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ ConfArch.IdentityProvider/Startup.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using ConfArch.IdentityProvider.Areas.Identity.Data;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfArch.IdentityProvider
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(
+                    Configuration.GetSection("IdentityServer:IdentityResources"))
+                .AddInMemoryApiResources(
+                    Configuration.GetSection("IdentityServer:ApiResources"))
+                .AddInMemoryClients(
+                    Configuration.GetSection("IdentityServer:Clients"))
+                .AddDeveloperSigningCredential()
+                .AddAspNetIdentity<ApplicationUser>();                        
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
